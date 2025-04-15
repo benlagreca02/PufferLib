eb1977d37f3396afdc4af72244185f2885fbe749
Benjamin LaGreca <benlagreca02@gmail.com>
2025-04-14 20:38:24 -0400

    changed dims, was too drastic, made taper from CNN hidden 'smoother'

--- pufferlib/environments/metroid_ii/torch.py
+++ pufferlib/environments/metroid_ii/torch.py
@@ -8,14 +8,14 @@ from metroid_env import *
 
 class Recurrent(pufferlib.models.LSTMWrapper):
     def __init__(self, env, policy,
-            input_size=1024, hidden_size=1024, num_layers=1):
+            input_size=512, hidden_size=512, num_layers=1):
         super().__init__(env, policy,
             input_size, hidden_size, num_layers)
 
 class Policy(nn.Module):
     # This is all horribly hacked
     def __init__(self, env, *args, framestack=1, flat_size=(4036),
-            input_size=512, hidden_size=1024, output_size=512,  # input_size doesn't really get used for my code oops
+            input_size=512, hidden_size=512, output_size=512,  # input_size doesn't really get used for my code oops
             channels_last=True, downsample=1, **kwargs):
         super().__init__()
         self.channels_last = channels_last
@@ -35,9 +35,9 @@ class Policy(nn.Module):
 
         # The network for encoding all features, three layers cause why not
         self.feature_network = nn.Sequential(
-            pufferlib.pytorch.layer_init(nn.Linear(flat_size, hidden_size)),
+            pufferlib.pytorch.layer_init(nn.Linear(flat_size, flat_size//2)),
             nn.ReLU(),
-            pufferlib.pytorch.layer_init(nn.Linear(hidden_size, hidden_size)),
+            pufferlib.pytorch.layer_init(nn.Linear(flat_size//2, hidden_size)),
             nn.ReLU(),
             pufferlib.pytorch.layer_init(nn.Linear(hidden_size, hidden_size)),
             nn.ReLU(),
